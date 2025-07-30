73ec6c4483969889e8a301dfea7778f8c87a1f8b
BlooshedDev <anggerbagas.s@gmail.com>
2025-07-30 22:04:26 +0700

    CI Bugfixes

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.blue,
-          // brightness: Brightness.light,
+          brightness: Brightness.light,
         ),
       ),
       home: MainPage(),
