257b835d3fd55ffaec259c95fa658979ca115ea6
BlooshedDev <anggerbagas.s@gmail.com>
2025-07-30 22:55:56 +0700

    Icons + Orientation Lock

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:b_clips/firebase_options.dart';
 import 'package:b_clips/views/page_control.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
