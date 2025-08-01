fe3f0bca969a744b29b4e7dc5f536f45d5c7003a
Blooshed Dev <anggerbagas.s@gmail.com>
2025-08-01 13:29:44 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:b_clips/views/page_control.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter/services.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
